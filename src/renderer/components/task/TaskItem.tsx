be28f15a27f51b672758dd85db53c2676278e32c
Ashik-AD <looge321@gmail.com>
2023-06-25 21:00:18 +0545

    finish ui stuff

--- src/renderer/components/task/TaskItem.tsx
+++ src/renderer/components/task/TaskItem.tsx
@@ -1,8 +1,8 @@
 /**
  * @params {bulletNo, id, title, description, duration, handleUpldateDescription()}
  */
-import {Icon} from '@iconify/react'
-import style from './style.module.scss'
+import { Icon } from "@iconify/react";
+import style from "./style.module.scss";
 function TaskItem() {
   const props = {
     id: 1,
@@ -14,18 +14,30 @@ function TaskItem() {
       console.log("helllo world");
     },
   };
+
   return (
     <article className={style.task__item}>
       <span className="semiBold small">#{props.bulletNo}</span>
       <div className={`${style.task__details} h3`}>
         <h3 className={style.task__title}>{props.title}</h3>
-        <span className="small">Add description</span>
+        <span className="small pointer">Add description</span>
       </div>
-      <div>
-        <Icon icon='mdi:clock-outline' />
-        {props.duration}min
+      <div className={style.task__duration}>
+        <Icon icon="mdi:clock-outline" />
+        <span className="medium">
+          {calculateDuration(props.duration)}
+        </span>
       </div>
     </article>
   );
 }
-export default TaskItem
+export default TaskItem;
+function calculateDuration(duration: number) {
+  if (duration > 60) {
+    const hr = Math.floor(duration / 60);
+    const min = duration % 60;
+    return `${hr}${hr > 1 ? "hrs" : "hr"} ${min}${min > 1 ? "mins" : "min"
+      }`;
+  }
+  return `${duration}${duration > 1 ? "mins" : "min"}`;
+}
