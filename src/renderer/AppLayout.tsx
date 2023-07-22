a4271148be696391320e353afdf10323ff145b2a
Ashik-AD <looge321@gmail.com>
2023-07-22 13:31:55 +0545

    chore: fetch session at startup

--- src/renderer/AppLayout.tsx
+++ src/renderer/AppLayout.tsx
@@ -5,26 +5,39 @@ import fetcher from "../lib/fetch";
 import Navigation from "./components/nav/Navigation";
 import Style from "./layout.module.scss";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useTaskStore } from "./store";
+import useStore from "./store";
+import { toast } from "react-hot-toast";
 
 function AppLayout({ children }: { children: ReactNode }) {
-  const { addList, addCurrent } = useTaskStore(
+  const { tasks, sessions } = useStore();
+  const { addTaskList, addCurrentTask } = tasks(
     (state) => ({
-      addList: state.addList,
-      addCurrent: state.addCurrent,
+      addTaskList: state.addList,
+      addCurrentTask: state.addCurrent,
     }),
     shallow,
   );
+  const addSessionList = sessions((state) => state.addLists, shallow);
 
   useEffect(() => {
-    fetcher("/tasks/today").then((res) => {
-      addList(res.tasks);
-      addCurrent();
-    })
-      .catch((err) => {
-        console.log(err);
-      });
+    (async function () {
+      const [taskList, sessionList] = await Promise.all([
+        await fetcher("/tasks/today"),
+        await fetcher("/sessions"),
+      ]);
+
+      const netError = taskList.error || sessionList.error;
+      if (netError) {
+        toast.error(netError);
+        console.log(netError);
+      }
+
+      addTaskList(taskList.tasks);
+      addCurrentTask();
+      addSessionList(sessionList);
+    })();
   }, []);
+
   return (
     <Router>
       <div className={Style.app__layout}>
