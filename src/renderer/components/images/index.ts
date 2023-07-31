b08450b4b1d92d1abe6e9e5e4c7ba02d79198fa6
Ashik-AD <looge321@gmail.com>
2023-07-31 17:36:14 +0545

    chore: image comonent

---
+++ src/renderer/components/images/index.ts
@@ -0,0 +1,19 @@
+
+import morning from "../../images/thumbnail_morning.png";
+import afternoon from "../../images/thumbnail_afternoon.png";
+import evening from "../../images/thumbnail_evening.png";
+import night from "../../images/thumbnail_night.png";
+
+export type Images = {
+  morning: string;
+  afternoon: string;
+  evening: string;
+  night: string;
+};
+const images: Images = {
+  morning,
+  afternoon,
+  evening,
+  night,
+};
+export default images;
