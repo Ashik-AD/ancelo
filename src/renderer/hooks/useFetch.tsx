64dfc5f76ea6a3695711be2857bb296368aa79f2
Ashik-AD <looge321@gmail.com>
2023-08-01 11:03:03 +0545

    chore: return data type will be any

--- src/renderer/hooks/useFetch.tsx
+++ src/renderer/hooks/useFetch.tsx
@@ -2,7 +2,7 @@ import fetcher from "lib/fetch";
 import { useEffect, useState } from "react";
 
 export default function useFetch(url: string, option?: RequestInit){
-  const [data, setData] = useState()
+  const [data, setData] = useState<any | null>(null)
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null)
 
