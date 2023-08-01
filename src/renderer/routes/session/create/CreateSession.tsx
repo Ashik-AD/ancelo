dbaf164d4491ff7ebf32eda729a0f64b9ba07163
Ashik-AD <looge321@gmail.com>
2023-08-01 11:03:59 +0545

    chore: add to session store

--- src/renderer/routes/session/create/CreateSession.tsx
+++ src/renderer/routes/session/create/CreateSession.tsx
@@ -1,39 +1,52 @@
 import fetcher from "lib/fetch";
 import { useRef } from "react";
 import { toast } from "react-hot-toast";
-import SessionForm, { SessionFormState } from "renderer/components/session/SessionForm";
+import { useNavigate } from "react-router-dom";
+import SessionForm, {
+  SessionFormState,
+} from "renderer/components/session/SessionForm";
 import SelectTasks from "renderer/components/task/SelectTasks";
 import type { SelectTasksHandle } from "renderer/components/task/SelectTasks";
 import useFetch from "renderer/hooks/useFetch";
+import { useAppStore } from "renderer/store";
+import { shallow } from "zustand/shallow";
 
 export default function CreateSession() {
+  const addSession = useAppStore(
+    ({ sessions }) => sessions.getState().addSessions,
+    shallow,
+  );
   const selectRef = useRef<SelectTasksHandle>(null);
-  const {data, isLoading, error} = useFetch('/tasks')
+  const { data, isLoading, error } = useFetch("/tasks");
 
-  const handleCreateSession = async (session: SessionFormState ) => {
-    const selectedTasks = selectRef.current?.selected()
-   
-    const res = await fetcher('/sessions', {
-      method: 'POST',
+  const router = useNavigate();
+
+  const handleCreateSession = async (session: SessionFormState) => {
+    const selectedTasks = selectRef.current?.selected();
+
+    const res = await fetcher("/sessions", {
+      method: "POST",
       body: JSON.stringify({
         ...session,
-        tasks: selectedTasks
-      })
-    })
+        tasks: selectedTasks,
+      }),
+    });
 
-    if(res.error){
-      toast.error(res.error)
-      console.log(res.error)
+    if (res.error) {
+      toast.error(res.error);
+      console.log(res.error);
       return;
     }
 
-    toast.success(`${res.session.title} session created successfully`)
-  }
+    addSession(res.session);
+    toast.success(`${res.session.title} session created successfully`);
+    router("../", { relative: "path" });
+  };
 
   return (
     <div>
       <SessionForm onCreate={handleCreateSession} />
-      <SelectTasks tasks={data?.tasks!!} ref={selectRef}/>
+      <SelectTasks tasks={data?.tasks!!} ref={selectRef} />
     </div>
   );
 }
