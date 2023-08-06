e63f277b0d4b99afd5c5ffff74ca3f958ad69df8
Ashik-AD <looge321@gmail.com>
2023-08-06 11:03:55 +0545

    chore: add setting

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -1,16 +1,19 @@
-import { create } from "zustand";
+import { createWithEqualityFn } from "zustand/traditional";
 import { SessionSlice, sessionSlice } from "./sessions";
 import { TaskSlice, taskSlice } from "./tasks";
+import { SettingSlice, settingSlice } from "./settings";
 
 export interface Store {
   tasks: TaskSlice;
   sessions: SessionSlice;
+  settings: SettingSlice;
 }
 function useStore() {
   return ({
     tasks: taskSlice,
     sessions: sessionSlice,
+    settings: settingSlice,
   });
 }
 
-export const useAppStore = create(() => useStore());
+export const useAppStore = createWithEqualityFn(() => useStore(), Object.is);
