43a02e2f125b51cb5069f232c7538c2de7682420
Ashik-AD <looge321@gmail.com>
2023-08-07 14:55:50 +0545

    chore: add mini task

--- src/renderer/Layout.tsx
+++ src/renderer/Layout.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
 import Style from "./layout.module.scss";
 import PlayAlert from "./components/play-alert/PlayAlert";
+import MiniTask from "./components/task/MiniTask";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={Style.root__layout}>
       <div className={Style.side__bar}>
+        <MiniTask />
         <h2>Music Player Bar</h2>
       </div>
       <div className={Style.main__content}>
