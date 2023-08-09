2b74fd68108c2a9a653d4a433948ee23a9b28597
Ashik-AD <looge321@gmail.com>
2023-08-09 14:56:36 +0545

    add routin form and create routine task

---
+++ src/renderer/components/routine/CreateRoutineTask.tsx
@@ -0,0 +1,109 @@
+import { ChangeEvent, forwardRef, useImperativeHandle, useState } from "react";
+import TaskList from "../task/TaskList";
+import TaskItem, { TaskItemProps } from "../task/TaskItem";
+import Input from "../form/Input";
+export type RoutineTaskState = TaskItemProps;
+
+export type RoutineTaskRef = {
+  getRoutineTask: () => RoutineTaskState[];
+};
+
+const CreateRoutineTask = forwardRef<RoutineTaskRef>((_, ref) => {
+  const [tasks, setTasks] = useState<RoutineTaskState[]>([]);
+
+  useImperativeHandle(ref, () => {
+    return {
+      getRoutineTask: () => tasks,
+    };
+  }, [tasks]);
+
+  const handleAddTask = (task: RoutineTaskState) => {
+    var taskList = tasks.slice();
+    taskList.push(task);
+    setTasks(taskList);
+  };
+  return (
+    <div>
+      <TaskList list={tasks} />
+      <TaskForm onCreate={handleAddTask} />
+    </div>
+  );
+});
+
+export default CreateRoutineTask;
+
+function TaskForm(
+  { onCreate }: {
+    onCreate: (task: RoutineTaskState) => void;
+  },
+) {
+  const [task, setTask] = useState<RoutineTaskState | null>(null);
+
+  const handleInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = evt.target;
+    setTask((prevTask) => ({
+      ...prevTask!!,
+      [name]: value,
+    }));
+  };
+
+  const genUniqueId = () =>
+    Math.floor(Math.random() * Date.now() / 1000).toString();
+  const handleCreateTask = () => {
+    if (task) return;
+    setTask({
+      title: "",
+      duration: 25,
+      id: genUniqueId(),
+    });
+  };
+
+  const handleSubmitTask = () => {
+    if (task?.title && task?.duration >= 5) {
+      onCreate(task);
+      setTask({
+        title: "",
+        duration: 25,
+        id: "",
+      });
+      return;
+    }
+  };
+
+  return (
+    <div>
+      {task && (
+        <TaskItem
+          {...task}
+          title={task?.title || "#task name"}
+        />
+      )}
+      <div className="flex">
+        <Input
+          type="text"
+          name="title"
+          label="Title"
+          role="button"
+          value={task?.title || ""}
+          placeholder="#task name"
+          onChange={handleInputChange}
+          onFocus={handleCreateTask}
+          onBlur={handleSubmitTask}
+          onKeyDown={(evt) => {
+            if (evt.key == "Enter") {
+              handleSubmitTask();
+              evt.preventDefault();
+            }
+          }}
+        />
+        <Input
+          type="number"
+          name="duration"
+          label="Duration"
+          value={task?.duration || 25}
+          onChange={handleInputChange}
+        />
+      </div>
+    </div>
+  );
+}
--- src/renderer/components/routine/RoutineForm.tsx
+++ src/renderer/components/routine/RoutineForm.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { ChangeEvent, ReactNode, useState } from "react";
 import Form from "../form/Form";
 import Input from "../form/Input";
-import SelectTime from "../form/SelectTime";
-import DayScheduleSelect from "../day-schedule/DayScheduleSelect";
+import SelectTime, { Time } from "../form/SelectTime";
+import DayScheduleSelect, {
+  DaySchedule,
+} from "../day-schedule/DayScheduleSelect";
 
 import style from "./style.module.scss";
 import ColorPicker from "../form/ColorPicker";
@@ -11,51 +13,105 @@ export type RoutineFormState = {
   title: string;
   schedule: string;
   breakDuration: number;
-  scheduleDay: string[];
+  scheduleDays: string[];
   theme: string;
 };
 export interface RoutineFormProps {
-  onSubmit: (values: RoutineFormState) => void;
+  onSubmit?: (values: RoutineFormState) => void;
   values?: RoutineFormState;
-  onCancel?: () => void;
-  isUpdate?: boolean;
+  children?: ReactNode;
 }
 
 export default function RoutineForm(
-  { values, onSubmit, onCancel, isUpdate }: RoutineFormProps,
+  { values, onSubmit, children }: RoutineFormProps,
 ) {
   const [inputs, setInputs] = useState<RoutineFormState>({
     schedule: values?.schedule || "",
     title: values?.title || "",
     theme: values?.theme || "",
     breakDuration: values?.breakDuration || 5,
-    scheduleDay: values?.scheduleDay || [],
+    scheduleDays: values?.scheduleDays || [],
   });
 
-  async function handleSubmit() {
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = evt.target;
+    setInputs(prevInputs => ({
+      ...prevInputs,
+      [name]: value
+    }))
+
   }
-  function handleSelectDays() {
+
+  async function handleSubmit() {
+    const { title, schedule, theme, breakDuration, scheduleDays } = inputs;
+
+    if (
+      !title?.trim() || !theme?.trim() || !schedule?.trim() || !breakDuration ||
+      scheduleDays?.length == 0
+    ) {
+      setError(`Can't create routine due to insufficient details.`);
+      return;
+    }
+    onSubmit && onSubmit(inputs);
+  }
+  function handleSelectDays(days: DaySchedule) {
+    setInputs((prevInputs) => ({ ...prevInputs, scheduleDays: days }));
+  }
+  function handlePickColor(color: string) {
+    setInputs((prevInputs) => ({ ...prevInputs, theme: color }));
+  }
+
+  function handleSelectSchedule(time: Time) {
+    setInputs(prevInputs => ({
+      ...prevInputs,
+      schedule: `${time.hours}:${time.minutes} ${time.meridiem}` 
+    }))
   }
   return (
-    <Form onSubmit={handleSubmit} className={style.routine__form}>
-      <div className={style.span__2}>
-        <Input
-          name="title"
-          type="text"
-          label="Title"
-          value={inputs.title}
-          placeholder="Read commic"
+    <>
+      {error && <p>{error}</p>}
+      <Form onSubmit={handleSubmit} className={style.routine__form}>
+        <div className={style.span__2}>
+          <Input
+            name="title"
+            type="text"
+            label="Title"
+            value={inputs.title}
+            placeholder="Read commic"
+            onChange={handleChange}
+          />
+        </div>
+        <div className={style.span__2}>
+          <DayScheduleSelect
+            onSelect={handleSelectDays}
+            selected={values?.scheduleDays}
+            label="Select Day"
+          />
+        </div>
+        <div className={`flex ${style.span__2}`}>
+          <SelectTime
+            onSelectTime={handleSelectSchedule}
+            label="Start time"
+          />
+          <Input
+            type="number"
+            name="breakDuration"
+            value={inputs.breakDuration}
+            label="Break Duration (minute)"
+            onChange={handleChange}
+          />
+        </div>
+        <ColorPicker
+          label="Theme"
+          onPick={handlePickColor}
+          color={inputs.theme}
         />
-      </div>
-      <div className={style.span__2}>
-      <DayScheduleSelect
-        onSelect={handleSelectDays}
-        selected={values?.scheduleDay}
-          label="Select Day"
-      />
-      </div>
-      <SelectTime onSelectTime={() => {}} label="Start time" />
-      <ColorPicker label="Theme" />
-    </Form>
+        <div className={style.span__full}>
+          {children ? children : <button className="btn btn__primary">Create Routine</button>}
+        </div>
+      </Form>
+    </>
   );
 }
