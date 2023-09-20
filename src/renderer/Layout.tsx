e3a8026b14c496aeaa35d6b4a9d5a031d40828a9
Ashik-AD <looge321@gmail.com>
2023-09-20 12:53:12 +0545

    add navigation component

--- src/renderer/Layout.tsx
+++ src/renderer/Layout.tsx
@@ -3,14 +3,16 @@ import { Toaster } from "react-hot-toast";
 import Style from "./layout.module.scss";
 import PlayAlert from "./components/play-alert/PlayAlert";
 import MiniTask from "./components/task/MiniTask";
+import Sidebar from "./components/sidebar/Sidebar";
+import Navigation from "./components/nav/Navigation";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={Style.root__layout}>
-      <div className={Style.side__bar}>
+      <Sidebar>
+        <Navigation />
         <MiniTask />
-        <h2>Music Player Bar</h2>
-      </div>
+      </Sidebar>
       <div className={Style.main__content}>
         {children}
         <Toaster />
