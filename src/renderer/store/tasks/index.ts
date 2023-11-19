2a737cad77b39e24f3d762a29eee11bc7c1eabd6
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-19 09:22:27 +0545

    chore: add recent task

--- src/renderer/store/tasks/index.ts
+++ src/renderer/store/tasks/index.ts
@@ -1,13 +1,15 @@
-import { create } from "zustand";
-import { immer } from "zustand/middleware/immer";
-import {devtools} from 'zustand/middleware'
-import { Tasks } from "@prisma/client";
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+import { devtools } from 'zustand/middleware';
+import { Tasks } from '@prisma/client';
+import { QueueRecent } from 'lib/ds';
 interface TasksState {
   list: Tasks[];
   completed: Tasks[];
   current: Tasks | null;
   start: boolean;
   listId: string | undefined;
+  recent: Tasks[];
 }
 
 interface TasksAction {
@@ -20,44 +22,58 @@ interface TasksAction {
   addListId: (payload: string) => void;
 }
 export type TaskSlice = TasksState & TasksAction;
-export const taskSlice = create(devtools(immer<TaskSlice>((set) => ({
-  list: [],
-  completed: [],
-  current: null,
-  start: false,
-  listId: undefined,
-  addList: (payload) => set({ list: payload }),
-  addTask: (payload) =>
-    set((state) => {
-      state.list.push(payload);
-    }),
-  addCurrent: () =>
-    set((state) => {
-      state.current = state.list[0];
-      state.list = state.list.slice(1, state.list.length);
-    }),
-  addNext: () =>
-    set((state) => {
-      if (state.list.length == 0) {
-        state.completed.push(state.current!!);
-        state.current = null;
-        state.start = false;
-        return;
-      }
-      const currentTask = state.list.splice(0, 1);
-      state.completed.push(state.current!!);
-      state.current = currentTask[0];
-    }),
-  addStart: () =>
-    set((state) => {
-      state.start = !state.start;
-    }),
-  addCompleted: (payload) =>
-    set((state) => {
-      state.completed.push(payload);
-    }),
-  addListId: (payload) => set((state) => {
-    state.listId = payload;
-  })
-}))));
 
+const recentTasks = new QueueRecent<Tasks>();
+function addRecentTask(item: Tasks) {
+  recentTasks.enqueue(item);
+  return recentTasks.getList();
+}
+
+export const taskSlice = create(
+  devtools(
+    immer<TaskSlice>((set) => ({
+      list: [],
+      completed: [],
+      recent: recentTasks.getList(),
+      current: null,
+      start: false,
+      listId: undefined,
+      addList: (payload) => set({ list: payload }),
+      addTask: (payload) =>
+        set((state) => {
+          state.list.push(payload);
+        }),
+      addCurrent: () =>
+        set((state) => {
+          state.current = state.list[0];
+          state.recent = addRecentTask({ ...state.current });
+          state.list = state.list.slice(1, state.list.length);
+        }),
+      addNext: () =>
+        set((state) => {
+          if (state.list.length == 0) {
+            state.completed.push(state.current!!);
+            state.current = null;
+            state.start = false;
+            return;
+          }
+          const currentTask = state.list.splice(0, 1);
+          state.completed.push(state.current!!);
+          state.current = currentTask[0];
+          state.recent = addRecentTask({ ...state.current });
+        }),
+      addStart: () =>
+        set((state) => {
+          state.start = !state.start;
+        }),
+      addCompleted: (payload) =>
+        set((state) => {
+          state.completed.push(payload);
+        }),
+      addListId: (payload) =>
+        set((state) => {
+          state.listId = payload;
+        }),
+    }))
+  )
+);
