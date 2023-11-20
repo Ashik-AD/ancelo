67b51d3ae8256a3d15b4b4edee2c3fb0e16f288c
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-20 08:26:17 +0545

    chore: reduce font size of title

--- src/renderer/components/task/TaskItem.tsx
+++ src/renderer/components/task/TaskItem.tsx
@@ -1,9 +1,9 @@
 /**
  * @params {bulletNo, id, title, description, duration, handleUpldateDescription()}
  */
-import style from "./style.module.scss";
-import TaskDuration from "./TaskDuration";
-import { Tasks } from "@prisma/client";
+import style from './style.module.scss';
+import TaskDuration from './TaskDuration';
+import { Tasks } from '@prisma/client';
 
 export interface TaskItemProps extends Partial<Tasks> {
   title: string;
@@ -11,30 +11,40 @@ export interface TaskItemProps extends Partial<Tasks> {
   duration: number;
   bulletNo?: number;
   onUpdateDescription?: (id: string) => void;
-  varient?: "default" | "minimal";
+  variant?: 'default' | 'minimal';
 }
-function TaskItem(
-  { id, bulletNo, title, description, duration, onUpdateDescription, varient }:
-    TaskItemProps,
-) {
+function TaskItem({
+  id,
+  bulletNo,
+  title,
+  description,
+  duration,
+  onUpdateDescription,
+  variant,
+}: TaskItemProps) {
   return (
     <article
       tabIndex={0}
       aria-label="Today's your task"
       className={style.task__item}
     >
-      <span className="semiBold small">#{bulletNo}</span>
+      <span className="semiBold text-small">#{bulletNo}</span>
       <div className={`${style.task__details}`}>
-        <p className={`${ varient == 'minimal' ? 'text-lg' : 'h4' } ${style.task__title}`}>{title}</p>
-        {varient != "minimal" &&
-          (
-            <span
-              className="text-small pointer"
-              onClick={() => onUpdateDescription && onUpdateDescription(id)}
-            >
-              {description || "Add description"}
-            </span>
-          )}
+        <p
+          className={`${variant == 'minimal' ? 'text-lg' : 'h5'} ${
+            style.task__title
+          }`}
+        >
+          {title}
+        </p>
+        {variant != 'minimal' && (
+          <span
+            className="text-small pointer"
+            onClick={() => onUpdateDescription && onUpdateDescription(id)}
+          >
+            {description || 'Add description'}
+          </span>
+        )}
       </div>
       <TaskDuration duration={duration} />
     </article>
