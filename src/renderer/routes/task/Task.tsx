772dbcc8471ddead8f319331eb0b6fc760784f58
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-21 09:20:51 +0545

    chore: add recent-task component

--- src/renderer/routes/task/Task.tsx
+++ src/renderer/routes/task/Task.tsx
@@ -2,15 +2,19 @@ import CurrentTask from 'renderer/components/task/CurrentTask';
 import TaskList from 'renderer/components/task/TaskList';
 import { useAppStore } from 'renderer/store';
 import TaskBanner from 'renderer/components/task/notask-banner';
+import RouteNav from 'renderer/components/nav/RouteNav';
+import RecentTask from 'renderer/components/task/recent-task';
 function Task() {
   const { list, current } = useAppStore(({ tasks }) =>
     tasks(({ list, current }) => ({ list, current }))
   );
   return (
     <>
+      <RouteNav title="" />
       {!current && <TaskBanner />}
       <CurrentTask />
       <TaskList list={list} />
+      <RecentTask />
     </>
   );
 }
