0959ee0184911ea933d3cc7fc7877a5cc4044d28
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-21 08:55:25 +0545

    chore: add read/write for json file

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,6 +1,7 @@
 /* eslint import/prefer-default-export: off */
 import { URL } from 'url';
 import path from 'path';
+import fs from 'fs/promises';
 
 export function resolveHtmlPath(htmlFileName: string) {
   if (process.env.NODE_ENV === 'development') {
@@ -10,4 +11,29 @@ export function resolveHtmlPath(htmlFileName: string) {
     return url.href;
   }
   return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
+}
+type JsonReadWritePromise = Error | { write?: boolean; data?: string };
+export function JsonReadWrite(
+  filePath: string,
+  mode?: 'READ' | 'WRITE',
+  data?: any
+): Promise<JsonReadWritePromise> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const fsMode = mode ?? 'READ';
+      const resolvedPath = path.resolve(__dirname, filePath);
+      if (fsMode == 'READ') {
+        const fileContent = await fs.readFile(resolvedPath, {
+          encoding: 'utf8',
+        });
+
+        resolve({ data: fileContent ? JSON.parse(fileContent) : [] });
+      }
+      const serialized = JSON.stringify(data);
+      await fs.writeFile(resolvedPath, serialized);
+      resolve({ write: true });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
