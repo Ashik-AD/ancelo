627978e16746b3d573ebc449acfe1e8a42527650
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-21 09:16:19 +0545

    chore: get recent tasks

--- src/renderer/AppLayout.tsx
+++ src/renderer/AppLayout.tsx
@@ -1,24 +1,28 @@
-import { ReactNode } from "react";
-import { useEffect } from "react";
-import { shallow } from "zustand/shallow";
-import fetcher from "../lib/fetch";
-import Style from "./layout.module.scss";
-import { toast } from "react-hot-toast";
-import { useAppStore } from "./store";
+import { ReactNode } from 'react';
+import { useEffect } from 'react';
+import { shallow } from 'zustand/shallow';
+import fetcher from '../lib/fetch';
+import Style from './layout.module.scss';
+import { toast } from 'react-hot-toast';
+import { useAppStore } from './store';
 
 function AppLayout({ children }: { children: ReactNode }) {
-  const { setTasks, setSessions, setRotuines } = useAppStore( (state) => ({
-    setTasks: state.tasks.getState().addList,
-    setSessions: state.sessions.getState().addLists,
-    setRotuines: state.routines.getState().setRoutine
-  }), shallow);
+  const { setTasks, setSessions, setRoutine, setRecentTask } = useAppStore(
+    (state) => ({
+      setTasks: state.tasks.getState().addList,
+      setSessions: state.sessions.getState().addLists,
+      setRoutine: state.routines.getState().setRoutine,
+      setRecentTask: state.tasks.getState().setRecent,
+    }),
+    shallow
+  );
 
   useEffect(() => {
     (async function () {
-      const [ taskList, sessionList, routineList ] = await Promise.all([
-        await fetcher("/tasks/today"),
-        await fetcher("/sessions"),
-        await fetcher('/routines')
+      const [taskList, sessionList, routineList] = await Promise.all([
+        await fetcher('/tasks/today'),
+        await fetcher('/sessions'),
+        await fetcher('/routines'),
       ]);
 
       const netError = taskList.error || sessionList.error;
@@ -29,14 +33,22 @@ function AppLayout({ children }: { children: ReactNode }) {
 
       setTasks(taskList.tasks);
       setSessions(sessionList);
-      setRotuines(routineList.routines)
+      setRoutine(routineList.routines);
     })();
   }, []);
 
+  useEffect(() => {
+    window.electron.ipcRenderer.sendMessage('ipc-get-recent-task');
+    window.electron.ipcRenderer.on('ipc-get-recent-task', (args: any) => {
+      console.log(args?.data);
+      if (args || args.hasOwnProperty('data')) {
+        setRecentTask(args.data);
+      }
+    });
+  }, []);
+
   return (
-    <main className={`content__main ${Style.app__layout}`}>
-        {children}
-    </main>
+    <main className={`content__main ${Style.app__layout}`}>{children}</main>
   );
 }
 export default AppLayout;
