5a0660534109ec90f6ba3f60b5c4709ac62e0cbc
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-22 08:44:26 +0545

    chore: make fields private

--- src/lib/ds.ts
+++ src/lib/ds.ts
@@ -4,53 +4,52 @@
  */
 
 export class QueueRecent<T> {
-  private list: T[];
-  private static MAX_SIZE = 20;
+  #list: T[] = [];
+  static #MAX_SIZE = 20;
 
   constructor() {
-    this.list = [];
+    this.#list = [];
   }
 
   public enqueue(item: T) {
     if (!item) {
       throw new Error('Empty parameter provide');
     }
-
-    if (this.list.length > QueueRecent.MAX_SIZE) {
+    if (this.#list?.length > QueueRecent.#MAX_SIZE) {
       // remove 1st item (which is old)
       // push new item to the end of the queue
       this.dequeue();
     }
     // im here being optimistic bcz idk whether item have id or not :)
-    const duplicateItemIndex = this.list.findIndex(
+    const duplicateItemIndex = this.#list.findIndex(
       //@ts-ignore
       (ls: any) => ls['id'] === item['id']
     );
-    const newList = [...this.list];
+    const newList = [...this.#list];
     if (duplicateItemIndex != -1) {
       newList.splice(duplicateItemIndex, 1);
     }
     newList.push(item);
-    this.list = newList;
+    this.#list = newList;
     return item;
   }
 
   public dequeue() {
-    if (this.list.length <= 0) {
+    if (this.#list.length <= 0) {
       throw new Error("Can't remove from empty queue");
     }
 
-    const copyList = [...this.list];
+    const copyList = [...this.#list];
     const dequeuedItem = copyList.splice(0, 1);
-    this.list = copyList;
+    this.#list = copyList;
     return dequeuedItem;
   }
 
   public getList(): T[] {
-    return this.list.reverse();
+    return this.#list.reverse();
   }
 
   public setList(list: T[]) {
-    this.list = list;
+    this.#list = [...list];
   }
 }
