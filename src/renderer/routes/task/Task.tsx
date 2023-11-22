740fcf9188fa853192adba726724f3ca61ce61ed
ashik-ad <dev.ashikdhimal@gmail.com>
2023-11-22 20:25:43 +0545

    chore: add completed task

--- src/renderer/routes/task/Task.tsx
+++ src/renderer/routes/task/Task.tsx
@@ -4,6 +4,7 @@ import { useAppStore } from 'renderer/store';
 import TaskBanner from 'renderer/components/task/notask-banner';
 import RouteNav from 'renderer/components/nav/RouteNav';
 import RecentTask from 'renderer/components/task/recent-task';
+import CompletedTask from 'renderer/components/task/completed-task';
 function Task() {
   const { list, current } = useAppStore(({ tasks }) =>
     tasks(({ list, current }) => ({ list, current }))
@@ -15,6 +16,7 @@ function Task() {
       <CurrentTask />
       <TaskList list={list} />
       <RecentTask />
+      <CompletedTask />
     </>
   );
 }
