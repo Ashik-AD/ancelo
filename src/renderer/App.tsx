3358977507fb47ff86644c76415f6caecaa2c045
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-05 12:35:46 +0545

    chore: add dashboard route

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,19 +1,21 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.scss";
-import Layout from "./Layout";
-import AppLayout from "./AppLayout";
-import Task from "./routes/task/Task";
-import Session from "./routes/session/Session";
-import Routine from "./routes/routine/Routine";
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Layout from './Layout';
+import AppLayout from './AppLayout';
+import Task from './routes/task/Task';
+import Session from './routes/session/Session';
+import Routine from './routes/routine/Routine';
+import Dashboard from './routes/dashboard/dashboard';
 export default function App() {
   return (
     <Layout>
       <AppLayout>
         <Routes>
-          <Route index element={<Task />} />
-          <Route path="/index.html" element={<Task />} /> 
+          <Route index element={<Dashboard />} />
+          <Route path="/index.html" element={<Dashboard />} />
+          <Route path="/task/*" element={<Task />} />
           <Route path="/session/*" element={<Session />} />
-          <Route path='/routine/*' element={<Routine />} />
+          <Route path="/routine/*" element={<Routine />} />
         </Routes>
       </AppLayout>
     </Layout>
