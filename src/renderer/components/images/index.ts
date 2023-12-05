b4691bbceeaef2394a902f27eb9f9684ef9e69b0
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-05 12:38:25 +0545

    chore: add image path

--- src/renderer/components/images/index.ts
+++ src/renderer/components/images/index.ts
@@ -4,6 +4,11 @@ import evening from '../../images/thumbnail_evening.png';
 import night from '../../images/thumbnail_night.png';
 import girl from '../../../../assets/images/girl.png';
 import anima from '../../../../assets/images/anima.png';
+import greeting from '../../../../assets/images/scenery.png';
+import fire from '../../../../assets/icons/fire.png';
+import gym from '../../../../assets/icons/gym.png';
+import alert from '../../../../assets/icons/alert.png';
+import clock from '../../../../assets/icons/clock.png';
 
 export type Images = {
   morning: string;
@@ -12,6 +17,11 @@ export type Images = {
   night: string;
   girl: string;
   anima: string;
+  greeting: string;
+  alert: string;
+  fire: string;
+  clock: string;
+  gym: string;
 };
 const images: Images = {
   morning,
@@ -20,5 +30,10 @@ const images: Images = {
   night,
   girl,
   anima,
+  greeting,
+  fire,
+  gym,
+  clock,
+  alert,
 };
 export default images;
