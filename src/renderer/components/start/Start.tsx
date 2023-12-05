6efd6efdfc5f24fe081f79871b86258f6f8a12f2
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-05 12:45:26 +0545

    chore: cleanup & formatted

--- src/renderer/components/start/Start.tsx
+++ src/renderer/components/start/Start.tsx
@@ -1,9 +1,9 @@
-import { ReactNode, useCallback } from "react";
-import { Icon } from "@iconify/react";
+import { ReactNode, useCallback } from 'react';
+import { Icon } from '@iconify/react';
 
-import style from "./style.module.scss";
-import { useAppStore } from "renderer/store";
-import useProgress from "renderer/hooks/useProgress";
+import style from './style.module.scss';
+import { useAppStore } from 'renderer/store';
+import useProgress from 'renderer/hooks/useProgress';
 
 interface Props {
   onStart: () => void;
@@ -12,20 +12,19 @@ interface Props {
 }
 
 export default function Start({ children, started = false, onStart }: Props) {
-  const { startTask, isStarted } = useAppStore(
-    ({ tasks }) =>
-      tasks((task) => ({
-        startTask: task.addStart,
-        isStarted: task.start,
-        currentTask: task.current,
-        loadNextTask: task.addNext,
-      })),
+  const { startTask, isStarted } = useAppStore(({ tasks }) =>
+    tasks((task) => ({
+      startTask: task.addStart,
+      isStarted: task.start,
+      currentTask: task.current,
+      loadNextTask: task.addNext,
+    }))
   );
 
   const start = started || isStarted;
-  // running this hook let's me count progress globally and can sync progress automaticaly
+  // running this hook let's me count progress globally and can sync progress automatically
   // component that uses progressbar component even they're unmounted and sync when mounting
-   useProgress(start);
+  useProgress(start);
 
   let handleStart = useCallback(() => {
     onStart && onStart();
@@ -36,11 +35,13 @@ export default function Start({ children, started = false, onStart }: Props) {
       className={style.start__btn}
       onClick={!started ? handleStart : undefined}
     >
-      {children
-        ? children
-        : start
-        ? <Icon icon="solar:pause-bold-duotone" />
-        : <Icon icon="solar:play-bold" />}
+      {children ? (
+        children
+      ) : start ? (
+        <Icon icon="solar:pause-bold-duotone" />
+      ) : (
+        <Icon icon="solar:play-bold" />
+      )}
     </button>
   );
 }
