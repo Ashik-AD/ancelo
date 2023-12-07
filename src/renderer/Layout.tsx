b04256cd84d7e10a3d00cffef185586ed6e9660e
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-07 09:28:15 +0545

    chore: change main content height & width

--- src/renderer/Layout.tsx
+++ src/renderer/Layout.tsx
@@ -1,19 +1,20 @@
-import { ReactNode } from "react";
-import { Toaster } from "react-hot-toast";
-import Style from "./layout.module.scss";
-import PlayAlert from "./components/play-alert/PlayAlert";
-import MiniTask from "./components/task/MiniTask";
-import Sidebar from "./components/sidebar/Sidebar";
-import Navigation from "./components/nav/Navigation";
+import { ReactNode } from 'react';
+import { Toaster } from 'react-hot-toast';
+import Style from './layout.module.scss';
+import PlayAlert from './components/play-alert/PlayAlert';
+import Sidebar from './components/sidebar/Sidebar';
+import Navigation from './components/nav/Navigation';
+import useTheme from './hooks/useTheme';
 
 function Layout({ children }: { children: ReactNode }) {
+  const { theme } = useTheme();
+  const themeClass = theme == 'dark' ? 'theme_dark' : 'theme_light';
   return (
-    <div className={Style.root__layout}>
+    <div className={`${Style.root__layout} ${themeClass}`}>
       <Sidebar>
         <Navigation />
-        <MiniTask />
       </Sidebar>
-      <div className={Style.main__content}>
+      <div className={`main_content ${Style.main__content}`}>
         {children}
         <Toaster />
         <PlayAlert />
