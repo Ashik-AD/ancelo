31680176cdf1475cb29cb9c79dd0df6d847645a1
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-07 09:26:36 +0545

    chore: theme style

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import './theme.scss';
+
 import Layout from './Layout';
 import AppLayout from './AppLayout';
 import Task from './routes/task/Task';
