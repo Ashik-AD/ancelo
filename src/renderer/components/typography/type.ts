4226af33032de8455e02da498c5cb014064e4d46
ashik-ad <dev.ashikdhimal@gmail.com>
2023-12-08 20:19:56 +0545

    chore: use base type

--- src/renderer/components/typography/type.ts
+++ src/renderer/components/typography/type.ts
@@ -1,19 +1,15 @@
-import { ReactNode } from 'react';
-import type { HTMLAttributes } from 'react'
-export type TextSize = 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
-export type HeadingVariant = 'h1' | 'h2' | 'h3' | 'h4'| 'h5' | 'h6';
+import { ReactNode } from "react";
+import type { HTMLAttributes } from "react";
+export type TextSize = "xs" | "s" | "m" | "l";
+export type HeadingVariant = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
-export interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
-  children: string | number;
+interface Base<T> extends HTMLAttributes<T> {
+  children: ReactNode;
   size?: TextSize;
 }
+export type ParagraphProps = Base<HTMLParagraphElement>;
+export type LabelProps = Base<HTMLSpanElement>;
 
-export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
-  children: ReactNode;
-}
+export type HeadingProps = Omit<Base<HTMLHeadingElement>, 'size'>;
 
-export interface TitleProps extends HTMLAttributes<HTMLSpanElement> {
-  size?: Exclude<TextSize, 'xl' | 'xxl'>;
-  children: ReactNode;
-}
-
+export type TitleProps = Base<HTMLSpanElement>;
